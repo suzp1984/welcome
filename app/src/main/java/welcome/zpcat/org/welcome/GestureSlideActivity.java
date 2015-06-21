b2feaef683ece7b7b625c40682fd17ba410ad2fe
suzp1984 <suzp1984@gmail.com>
2015-06-21 16:35:15 +0800

    add simple guesture

--- app/src/main/java/welcome/zpcat/org/welcome/GestureSlideActivity.java
+++ app/src/main/java/welcome/zpcat/org/welcome/GestureSlideActivity.java
@@ -2,12 +2,28 @@ package welcome.zpcat.org.welcome;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.GestureDetector;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
 
-public class GestureSlideActivity extends Activity {
+import welcome.zpcat.org.welcome.util.SimpleGestureFilter;
+
+public class GestureSlideActivity extends Activity implements SimpleGestureFilter.SimpleGestureListener {
+
+    private SimpleGestureFilter mSimpleGustureDetector;
+
+    private int[] mResources = {
+            R.drawable.guide_01,
+            R.drawable.guide_02
+    };
+
+    private int mIndex = 0;
+    private ImageView mGuide;
 
     // http://androidexample.com/Swipe_screen_left__right__top_bottom/index.php?view=article_discription&aid=95&aaid=118
     @Override
@@ -18,6 +34,9 @@ public class GestureSlideActivity extends Activity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gesture_slide);
+        mGuide = (ImageView) findViewById(R.id.guide_iv);
+
+        mSimpleGustureDetector = new SimpleGestureFilter(this, this);
     }
 
     @Override
@@ -40,5 +59,43 @@ public class GestureSlideActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        this.mSimpleGustureDetector.onTouchEvent(event);
+
+        return super.dispatchTouchEvent(event);
+    }
+
+    @Override
+    public void onSwipe(int direction) {
+        Log.e("TAG", " " + direction);
+
+        switch (direction) {
+            case SimpleGestureFilter.SWIPE_LEFT:
+                mIndex --;
+                mIndex += mResources.length;
+                mIndex = mIndex % 2;
+                break;
+            case SimpleGestureFilter.SWIPE_RIGHT:
+                mIndex++;
+                mIndex = mIndex % 2;
+                break;
+            case SimpleGestureFilter.SWIPE_UP:
+                break;
+            case SimpleGestureFilter.SWIPE_DOWN:
+                break;
+            default:
+                break;
+        }
+
+        mGuide.setBackgroundResource(mResources[mIndex]);
+        mGuide.refreshDrawableState();
+    }
+
+    @Override
+    public void onDoubleTap() {
+
     }
 }
